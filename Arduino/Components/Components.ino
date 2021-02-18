518ef9b3c6fa8e0c712342aadec516be8a45a96b
Andi <andi.scholz@gmail.com>
2021-02-18 17:50:38 +0100

    arrays arrays arrays

--- Arduino/Components/Components.ino
+++ Arduino/Components/Components.ino
@@ -4,6 +4,7 @@
 #include "MidiFader.h"
 #include "MidiController.h"
 #include "MidiButton.h"
+#include "AbstractHMI.h"
 #include "DebugController.h"
 #include "AbstractTransportController.h"
 ///avoid digital pin 52, this seems to be broken
@@ -47,27 +48,52 @@ MidiButton midiButton11(29, 61, midiController);
 MidiButton midiButton12(31, 62, midiController);
 MidiButton midiButton13(33, 63, midiController);
 
+MidiButton* testMidiButton;
+
+Button synchornizrButton(44);
+
+void SynchronizeAll();
+
+int componentArraySize;
+const int midiFaderCount = 13;
+AbstractHMI* midiFaderArray[midiFaderCount]; 
+
+const int midiButtonCount = 13;
+AbstractHMI* midiButtonArray[midiButtonCount]; 
 
 void setup() 
 {
-  midiController.begin();
+  midiController.begin();  
 
-  eqRotary0.begin();
-  eqRotary1.begin();
-  eqRotary2.begin();
-  eqRotary3.begin();
-  eqRotary4.begin();
-  eqRotary5.begin();  
-  eqRotary6.begin();  
-  eqRotary7.begin();  
-  eqRotary8.begin();  
-  eqRotary9.begin();  
-  eqRotary10.begin();  
-  eqRotary11.begin();  
-  eqRotary12.begin();  
-  eqRotary13.begin();  
+  for(int i = 0; i < midiFaderCount; i++){
+    int analogPin = A0 + i;
+    int midiControllNumber = 0 + i;
+    midiFaderArray[i] = new MidiFader(analogPin, midiControllNumber, midiController);
+    midiFaderArray[i]->begin();
+  }
 
+  for(int b = 0; b < midiButtonCount; b++){
+    int digitalPin = 22 + b;
+    int midiControllNumber = 50 + b;
+    midiButtonArray[b] = new MidiButton(digitalPin, midiControllNumber, midiController);
+    midiButtonArray[b]->begin();
+  }
 
+  // eqRotary0.begin();
+  // eqRotary1.begin();
+  // eqRotary2.begin();
+  // eqRotary3.begin();
+  // eqRotary4.begin();
+  // eqRotary5.begin();  
+  // eqRotary6.begin();  
+  // eqRotary7.begin();  
+  // eqRotary8.begin();  
+  // eqRotary9.begin();  
+  // eqRotary10.begin();  
+  // eqRotary11.begin();  
+  // eqRotary12.begin();  
+  // eqRotary13.begin(); 
+  
   midiButton0.begin();
   midiButton1.begin();
   midiButton2.begin();
@@ -82,25 +108,35 @@ void setup()
   midiButton11.begin();
   midiButton12.begin();
   midiButton13.begin();
+
+  synchornizrButton.begin();
 }
 
 
 void loop() 
 {
-  eqRotary0.read();
-  eqRotary1.read();
-  eqRotary2.read();
-  eqRotary3.read();
-  eqRotary4.read();
-  eqRotary5.read();  
-  eqRotary6.read();  
-  eqRotary7.read();  
-  eqRotary8.read();  
-  eqRotary9.read();  
-  eqRotary10.read();  
-  eqRotary11.read();  
-  eqRotary12.read();  
-  eqRotary13.read();  
+  // eqRotary0.read();
+  // eqRotary1.read();
+  // eqRotary2.read();
+  // eqRotary3.read();
+  // eqRotary4.read();
+  // eqRotary5.read();  
+  // eqRotary6.read();  
+  // eqRotary7.read();  
+  // eqRotary8.read();  
+  // eqRotary9.read();  
+  // eqRotary10.read();  
+  // eqRotary11.read();  
+  // eqRotary12.read();  
+  // eqRotary13.read();  
+ 
+  for(int i = 0; i < midiFaderCount; i++){
+    midiFaderArray[i]->read();
+  }
+
+  for(int b = 0; b < midiButtonCount; b++){
+    midiButtonArray[b]->read();
+  }
 
   midiButton0.read();
   midiButton1.read();
@@ -118,6 +154,12 @@ void loop()
   midiButton13.read();
 
 
+  synchornizrButton.read();
+
+  if(synchornizrButton.stateChanged() && synchornizrButton.isPressed()){
+    SynchronizeAll();
+  }
+
   // eqRotary1.read();
   // eqRotary2.read();
   // eqRotary3.read();
@@ -130,5 +172,41 @@ void loop()
 
  
   delay(100);        // delay in between reads for stability
+}
+
+void SynchronizeAll()
+{
+  #ifdef DEBUG
+  Serial.println("synchronizing all");
+  #endif
+  eqRotary0.sendMidiCommand();
+  eqRotary1.sendMidiCommand();
+  eqRotary2.sendMidiCommand();
+  eqRotary3.sendMidiCommand();
+  eqRotary4.sendMidiCommand();
+  eqRotary5.sendMidiCommand();  
+  eqRotary6.sendMidiCommand();  
+  eqRotary7.sendMidiCommand();  
+  eqRotary8.sendMidiCommand();  
+  eqRotary9.sendMidiCommand();  
+  eqRotary10.sendMidiCommand();  
+  eqRotary11.sendMidiCommand();  
+  eqRotary12.sendMidiCommand();  
+  eqRotary13.sendMidiCommand();  
+
+  midiButton0.sendMidiCommand();
+  midiButton1.sendMidiCommand();
+  midiButton2.sendMidiCommand();
+  midiButton3.sendMidiCommand();
+  midiButton4.sendMidiCommand();
+  midiButton5.sendMidiCommand();
+  midiButton6.sendMidiCommand();
+  midiButton7.sendMidiCommand();
+  midiButton8.sendMidiCommand();
+  midiButton9.sendMidiCommand();
+  midiButton10.sendMidiCommand();
+  midiButton11.sendMidiCommand();
+  midiButton12.sendMidiCommand();
+  midiButton13.sendMidiCommand();
 }
 
